0278e36404181d752e6a674227f22e65a632cebb
garyzhang2681 <garyzhang2681@gmailcom>
2014-09-29 13:52:30 +0800

    test
    test

---
+++ test1/test1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  test1
+//
+//  Created by Shaojun on 2014-09-29.
+//  Copyright (c) 2014 OrganizationName. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "shaojunAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([shaojunAppDelegate class]));
+    }
+}
---
+++ test1/test1/shaojunfffffff.h
@@ -0,0 +1,13 @@
+//
+//  shaojunfffffff.h
+//  test1
+//
+//  Created by Shaojun on 2014-09-29.
+//  Copyright (c) 2014 OrganizationName. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface shaojunfffffff : NSObject
+
+@end
